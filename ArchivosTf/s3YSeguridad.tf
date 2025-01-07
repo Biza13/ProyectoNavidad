85a54924318aad1ba06324c59e871ad2d986dc6f
Biza_13 <serrano_0_1@hotmail.com>
2025-01-07 17:56:36 +0100

    sin balanceador subredes extra ni listener

--- ArchivosTf/s3YSeguridad.tf
+++ ArchivosTf/s3YSeguridad.tf
@@ -11,14 +11,12 @@ resource "aws_security_group" "security_ecs" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    security_groups  = [aws_security_group.lb_security.id]  # Permitir tráfico de entrada solo desde el ALB
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    security_groups  = [aws_security_group.lb_security.id]  # Permitir tráfico de entrada solo desde el ALB
   }
   
   #egress reglas de salida
@@ -27,36 +25,6 @@ resource "aws_security_group" "security_ecs" {
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"] #permitir que los contenedores salgan a Internet a través de la NAT Gateway
-  }
-}
-
-# Crear un grupo de seguridad para ALB
-resource "aws_security_group" "lb_security" {
-  name        = "alb-security"
-  description = "Security group para ALB"
-  vpc_id      = aws_vpc.Desarrollo-web-VPC.id
-
-  # Permitir tráfico HTTP/HTTPS desde cualquier IP
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Permitir tráfico HTTP desde cualquier IP
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Permitir tráfico HTTPS desde cualquier IP
-  }
-
-  # Regla de salida (permitir todo el tráfico)
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
--- ArchivosTf/vpcYSubredes.tf
+++ ArchivosTf/vpcYSubredes.tf
@@ -67,43 +67,26 @@ resource "aws_route_table_association" "rt-asociacion-publica" {
   route_table_id = aws_route_table.public-rt.id
 }
 
-# Subred pública en AZ2
-resource "aws_subnet" "subred-publica-az2" {
-  vpc_id                  = aws_vpc.Desarrollo-web-VPC.id
-  cidr_block              = var.cidrSubredPublicaAz
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-  tags = {
-    "Name" = "subred-publica-az2"
-  }
-}
-
-# Asociar la tabla de enrutamiento a la subred pública de AZ2
-resource "aws_route_table_association" "rt-asociacion-publica-az2" {
-  subnet_id = aws_subnet.subred-publica-az2.id
-  route_table_id = aws_route_table.public-rt.id
-}
-
 #--------------------FIN SUBRED PÚBLICA--------------------
 
 #--------------------SUBRED PRIVADA--------------------
 #subred privada
-resource "aws_subnet" "subred-privada" {
+/* resource "aws_subnet" "subred-privada" {
   vpc_id = aws_vpc.Desarrollo-web-VPC.id
   cidr_block = "10.0.1.0/24"
   tags = {
     "Name" = "subred-privada"
   }
-}
+} */
 
 #Crear un ip elastica para la NAT gateway
-resource "aws_eip" "NAt-gateway" {
+/* resource "aws_eip" "NAt-gateway" {
   depends_on = [ aws_route_table_association.rt-asociacion-publica ]
   domain = "vpc"
-}
+} */
 
 #crear la nat
-resource "aws_nat_gateway" "nat-gateway" {
+/* resource "aws_nat_gateway" "nat-gateway" {
   allocation_id = aws_eip.NAt-gateway.id
   subnet_id     = aws_subnet.subred-publica.id  //se pone la subred publica para que tenga acceso hacia afuera
 
@@ -111,10 +94,10 @@ resource "aws_nat_gateway" "nat-gateway" {
     Name = "NAT gateway"
   }
   depends_on = [aws_internet_gateway.igw]
-}
+} */
 
 #creamos la tabla de rutas para la nat gateway
-resource "aws_route_table" "nat-gateway-rt" {
+/* resource "aws_route_table" "nat-gateway-rt" {
     //depends_on = [ aws_nat_gateway.nat-gateway ]
   vpc_id = aws_vpc.Desarrollo-web-VPC.id
 
@@ -126,13 +109,13 @@ resource "aws_route_table" "nat-gateway-rt" {
   tags = {
     Name = "Tabla Enrutamiento para el NAT gateway"
   }
-}
+} */
 
 #asociar la tabla de enrutamiento con el nat gateway
-resource "aws_route_table_association" "rt-asociacion-NAT" {
+/* resource "aws_route_table_association" "rt-asociacion-NAT" {
     //depends_on = [ aws_route_table.nat-gateway-rt ]
   subnet_id = aws_subnet.subred-privada.id
   route_table_id = aws_route_table.nat-gateway-rt.id
-}
+} */
 
 #--------------------FIN SUBRED PRIVADA--------------------
