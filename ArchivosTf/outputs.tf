7a05eb17b9b63a05b5f09aadcd479f1141e813ba
Biza_13 <serrano_0_1@hotmail.com>
2025-01-11 16:16:20 +0100

    pruebas

--- ArchivosTf/outputs.tf
+++ ArchivosTf/outputs.tf
@@ -10,3 +10,8 @@ output "s3" {
   description = "Nombre del bucket"
   value = aws_s3_bucket.s3.id
 }
+
+output "instance_public_ip" {
+  description = "IP publica de instancia EC2"
+  value = aws_instance.instancia.public_ip
+}
